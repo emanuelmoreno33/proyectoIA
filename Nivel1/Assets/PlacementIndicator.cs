a8fb42c7d745a5edeec4d0566be137b67548c959
Marcos Gonzalez <marcos.salgado17@tectijuana.edu.mx>
2020-11-17 20:16:21 -0800

    Version 0.1

---
+++ Nivel1/Assets/PlacementIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start()
+    {
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        //hide placement visual
+        visual.SetActive(false);
+    }
+
+    void Update()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width/2,Screen.height /2),hits,TrackableType.Planes);
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+            }
+
+        }
+    }
+}
---
+++ Nivel1/Assets/Spam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spam : MonoBehaviour
+{
+    public GameObject objectoToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectoToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+
+        }
+    }
+}
