7d20f1cb75a758db64249530c8c879bbd4e856d2
emanuelmoreno33 <emanuel.moreno16@tectijuana.edu.mx>
2020-11-29 21:42:50 -0800

    interfaz

---
+++ Nivel1/Assets/live.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class live : MonoBehaviour
+{
+    public Sprite[] corazones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CambioVida(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+public void CambioVida(int pos)
+    {
+        this.GetComponent<Image>().sprite = corazones[pos];
+
+
+    }
+}
